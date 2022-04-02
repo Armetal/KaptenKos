33125d4f9987b7f6ab2dd35277e3ecf96d62f48a
AhmadZRizqi <az.rizqi@gmail.com>
2022-04-02 09:18:06 +0700

    menghubungkan menu dengan settings

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan1/settings.dart';
 
 class MenuPage extends StatelessWidget {
   @override
@@ -7,7 +8,20 @@ class MenuPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Menu"),
       ),
-      body: Center(),
+      body: Container(
+        width: 100,
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return Setting1();
+              }));
+            },
+            style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(255, 5, 92, 92)),
+            child: Text("Setting")),
+        padding: EdgeInsets.all(5.0),
+      ),
     );
   }
 }
