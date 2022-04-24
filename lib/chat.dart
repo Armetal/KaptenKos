89c58cb8ddbf4948eec5d94cb2d20c9a970b6d24
AhmadZRizqi <az.rizqi@gmail.com>
2022-04-24 21:38:52 +0700

    tambah bagian chat

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan1/chat_view.dart';
 import 'package:latihan1/menu_page.dart';
 
 //belum dipake
@@ -6,8 +7,33 @@ class Chat extends StatelessWidget {
   const Chat({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xffC6EAFF),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: ListView(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Card(
+            child: ListTile(
+              title: const Text("Bapak Kos"),
+              leading: Image.asset(
+                'assets/person.png',
+                width: 100,
+                height: 100,
+                fit: BoxFit.fitHeight,
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ChatView()),
+                );
+              },
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
---
+++ lib/chat_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:latihan1/menu_page.dart';
+
+//belum dipake
+class ChatView extends StatelessWidget {
+  const ChatView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xffC6EAFF),
+    );
+  }
+}
