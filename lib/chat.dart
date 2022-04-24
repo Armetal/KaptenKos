ef0de159a77dd423d4942621242ace331b908774
AhmadZRizqi <az.rizqi@gmail.com>
2022-04-24 22:34:55 +0700

    coba coba

--- lib/chat.dart
+++ lib/chat.dart
@@ -9,30 +9,34 @@ class Chat extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Chat"),
+            centerTitle: true,
+          ),
           body: ListView(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Card(
-            child: ListTile(
-              title: const Text("Bapak Kos"),
-              leading: Image.asset(
-                'assets/person.png',
-                width: 100,
-                height: 100,
-                fit: BoxFit.fitHeight,
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ChatView()),
-                );
-              },
-            ),
-          ),
-        ],
-      )),
+              Card(
+                child: ListTile(
+                  title: const Text("Bapak Kos"),
+                  leading: Image.asset(
+                    'assets/person.png',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ChatView()),
+                    );
+                  },
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
