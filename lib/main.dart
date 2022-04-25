5d885a07b4fb1c52441dbd31a5ecf764eb99ec7d
Noval Alamsyah <alamsyahnoval20@gmail.com>
2022-04-25 19:52:47 +0700

    perbaikan

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:latihan1/chat.dart';
-import 'package:latihan1/home.dart';
-import 'package:latihan1/payment.dart';
-import 'package:latihan1/profile.dart';
-import 'package:latihan1/search.dart';
-import 'package:latihan1/settings.dart';
+import 'package:latihan1/pembukaan.dart';
 
-class MenuPage extends StatefulWidget {
-  const MenuPage({Key? key}) : super(key: key);
-
-  @override
-  State<MenuPage> createState() => _MenuPage();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MenuPage extends State<MenuPage> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    HomePage(),
-    Search(),
-    Chat(),
-    Payment(),
-    Profile(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Menu'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'KaptenKos',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: 'Chat',
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.payment),
-            label: 'Payment',
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-            backgroundColor: Colors.blue,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.black,
-        onTap: _onItemTapped,
-      ),
+      home: pembukaan(),
     );
   }
 }
